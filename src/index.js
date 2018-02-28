8a2cf86afff59291e73651537ff8cd800613f087
Yana <YanaLSoltan@gmail.com>
2018-02-28 22:05:35 +0300

    task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+//module.exports =
+    function getZerosCount(number) {
+
+    const fives = 5;
+
+    var count5 = 0;
+    var count2 = 0;
+
+
+    do{
+        if(number % fives === 0) {
+            count5 +=  number/fives;
+
+        }
+
+    }
+     while (number!= 0)
+
+        return count5 + count2;
 }
