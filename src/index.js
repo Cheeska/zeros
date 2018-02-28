be780faff48b4b0b09a1dfaf2a9bdbac086636bd
Yana <YanaLSoltan@gmail.com>
2018-02-28 22:50:40 +0300

    task 7

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
+
 //module.exports =
-    function getZerosCount(number) {
+        function getZerosCount(number) {
 
-    const fives = 5;
+            const fives = 5;
 
-    var count5 = 0;
-    var count2 = 0;
+            var count5 = 0;
+            var count2 = 0;
 
 
-    do{
-        if(number % fives === 0) {
-            count5 +=  number/fives;
+            do{
+                if(number % fives === 0) {
+                    count5 +=  number/fives;
+                    number = number/fives;
 
+                }
+
+            }
+            while (number!= 0)
+
+            return count5 + count2;
         }
 
-    }
-     while (number!= 0)
 
-        return count5 + count2;
-}
+
+console.log(getZerosCount(84583674));
